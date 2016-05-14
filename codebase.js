fa3e6d3b83e185424abd66ab6b7d6158367694bd
EvanSchallerer <schallee@oregonstate.edu>
2016-05-13 23:59:46 -0700

    Initial commit of project

---
+++ codebase.js
@@ -0,0 +1,79 @@
+apiURL = 'https://api.github.com/repos/';
+user = 'EvanSchallerer';//'goForkYourself/';
+myRepo = '381CS';//'CS290_example/';
+
+function getRequest(payload, func){
+    var req = new XMLHttpRequest();
+    req.open('GET', payload, true);
+    req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.responseText);
+        func(response);
+      } else {
+        func(null);
+      }});
+    req.send(null);
+    event.preventDefault();
+}
+
+document.getElementById('getOneFile').addEventListener('click', function(event){
+    var filePath = document.getElementById('filename').value; 
+    if(filePath === null){
+        alert('Please enter a file path.');
+    } else{
+        var url = apiURL+user+myRepo+'contents/' + filePath;
+        getRequest(filePath, getSingleFile);
+    }
+});
+
+function getSingleFile(file){
+    clearNode('allIssues');
+    var fileElem = document.getElementById('singleFile'); 
+    if(file === null){
+        file.innerHTML = 'The specified file was not found.'
+    } else{
+        fileElem.appendChild(createFileNode(file));        
+    }   
+}
+
+function createFileNode(file){
+    var fileNode = document.createElement('div');
+    
+    var fSize = document.createElement('div');
+    fSize.innerHTML = 'File size: ' + file.size;
+    fileNode.appendChild(fSize);
+    // TODO: NOT DONE
+    var txt = downloadFileContents(file.download_url);
+    if(txt === null){ return; }
+    
+    var pre = document.createElement('pre');
+    var code = document.createElement('code');
+    
+    pre.appendChild(code);
+    fileNode.appendChild(pre);
+    return pre;
+}
+
+/* SOURCE: http://stackoverflow.com/questions/6375461/get-html-code-using-javascript-with-a-url  */ 
+function makeHttpObject() {
+        try {return new XMLHttpRequest();}
+        catch (error) {}
+        try {return new ActiveXObject("Msxml2.XMLHTTP");}
+        catch (error) {}
+        try {return new ActiveXObject("Microsoft.XMLHTTP");}
+        catch (error) {}
+
+        throw new Error("Could not create HTTP request object.");
+}
+/* SOURCE: http://stackoverflow.com/questions/6375461/get-html-code-using-javascript-with-a-url  */ 
+function downloadFileContents(url){
+    var request = makeHttpObject();
+    request.open("GET", "your_url", false);
+    request.send(null);
+    request.onreadystatechange = function() {
+    if (request.readyState == 4)
+        // TODO: Not Done
+        alert(request.responseText);
+    };
+}
+//   2hwCS381/MiniLogo.schallee.hs
---
+++ issues.js
@@ -0,0 +1,83 @@
+apiURL = 'https://api.github.com/repos/';
+user = 'goForkYourself/';
+myRepo = 'CS290_example/';
+
+function getRequest(payload, func){
+    var req = new XMLHttpRequest();
+    req.open('GET', payload, true);
+    req.addEventListener('load',function(){
+      if(req.status >= 200 && req.status < 400){
+        var response = JSON.parse(req.responseText);
+        func(response);
+      } else {
+        func(null);
+      }});
+    req.send(null);
+    event.preventDefault();
+}
+
+document.getElementById('getAllIssues').addEventListener('click', function(event){
+    var url = apiURL + user + myRepo + 'issues';
+    getRequest(url, addIssues);
+});
+
+document.getElementById('getOneIssue').addEventListener('click', function(event){
+    var num = document.getElementById('enteredIssueNum').value;
+    if(num < 1){ alert('Please enter a value greater than 0.');}
+    if(num != null && num > 0){
+        var url = apiURL + user + myRepo + 'issues/' + num;
+        getRequest(url, addSingleIssue);
+    }
+});
+
+function addSingleIssue(issue){
+    clearNode('allIssues');
+    var issueElem = document.getElementById('singleIssue');
+    if(issue === null){
+        issueElem.innerHTML = 'The specified issue was not found.'
+    } else{
+        issueElem.appendChild(createIssueNode(issue));
+    }
+}
+
+function createIssueNode(issue){
+    var issueElem = document.createElement('div');
+        
+    var issueNum = document.createElement('h3');
+    issueNum.innerHTML = 'Issue Number ' + issue.number;
+    issueElem.appendChild(issueNum);
+
+    var issueBody = document.createElement('div');
+    issueBody.innerHTML = 'Issue Body: ' + issue.body;
+    issueElem.appendChild(issueBody);
+
+    var issueState = document.createElement('div');
+    issueState.innerHTML = 'Issue State: ' + issue.state;
+    issueElem.appendChild(issueState);
+    
+    var issuemilestone = document.createElement('div');
+    issuemilestone.innerHTML = 'Issue Milestone: ' + issue.milestone;
+    issueElem.appendChild(issuemilestone);
+    return issueElem;
+}
+
+function addIssues(issues){
+    clearNode('allIssues');
+    var issuesElem = document.getElementById('allIssues');
+    issues.reverse().forEach(function (issue, i) {
+        issuesElem.appendChild(createIssueNode(issue));
+        issuesElem.appendChild(document.createElement('br'));
+    });
+    if(issues.length == 0){
+        var issueElem = document.createElement('div');
+        issueElem.innerHTML = 'There are no issues for this repository.';
+        issuesElem.appendChild(issueElem);
+    }
+}
+
+function clearNode(elementId){
+    var node = document.getElementById(elementId);
+    while (node.firstChild) {
+        node.removeChild(node.firstChild);  
+    }
+}
