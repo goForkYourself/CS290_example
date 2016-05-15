29008ec76625476f4ab02ee382e9295b1385d64e
EvanSchallerer <schallee@onid.oregonstate.edu>
2016-05-14 19:33:14 -0700

    code fetching working

--- codebase.js
+++ codebase.js
@@ -1,12 +1,13 @@
 apiURL = 'https://api.github.com/repos/';
-user = 'EvanSchallerer';//'goForkYourself/';
-myRepo = '381CS';//'CS290_example/';
+user = 'goForkYourself/';
+myRepo = 'CS290_example/';
 
 function getRequest(payload, func){
     var req = new XMLHttpRequest();
     req.open('GET', payload, true);
     req.addEventListener('load',function(){
       if(req.status >= 200 && req.status < 400){
+        //console.log(req.responseText);
         var response = JSON.parse(req.responseText);
         func(response);
       } else {
@@ -22,32 +23,35 @@ document.getElementById('getOneFile').addEventListener('click', function(event){
         alert('Please enter a file path.');
     } else{
         var url = apiURL+user+myRepo+'contents/' + filePath;
-        getRequest(filePath, getSingleFile);
+        getRequest(url, getSingleFile);
     }
 });
 
 function getSingleFile(file){
-    clearNode('allIssues');
+    clearNode('singleFile');
     var fileElem = document.getElementById('singleFile'); 
     if(file === null){
         file.innerHTML = 'The specified file was not found.'
     } else{
+        console.log(file.content);
         fileElem.appendChild(createFileNode(file));        
     }   
 }
 
 function createFileNode(file){
     var fileNode = document.createElement('div');
-    
+    console.log(file.size);
     var fSize = document.createElement('div');
     fSize.innerHTML = 'File size: ' + file.size;
     fileNode.appendChild(fSize);
+    
     // TODO: NOT DONE
-    var txt = downloadFileContents(file.download_url);
-    if(txt === null){ return; }
+    // var txt = downloadFileContents(file.download_url);
+    // if(txt === null){ return; }
     
     var pre = document.createElement('pre');
     var code = document.createElement('code');
+    code.innerHTML = atob(file.content);//.toString('utf8'); 
     
     pre.appendChild(code);
     fileNode.appendChild(pre);
